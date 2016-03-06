50257a5fc6777a21eed910847dbd33cf767376ca
Ciprian Tarta <ciprian@cipriantarta.ro>
2016-03-06 19:39:49 +0200

    updates capture

--- capture.py
+++ capture.py
@@ -7,18 +7,26 @@ import sys
 
 
 class Streamer:
-    command = (
-        'ffmpeg',
-        '-f', 'avfoundation',
-        '-framerate', '30',
-        '-video_size', '640x480',
-        '-i', '0:0',
-        '-c:v', 'libx264',
-        '-crf', '18',
-        '-preset', 'ultrafast',
-        '-r', '30',
-        '-f', 'mpeg',
-        '-')
+    command = 'ffmpeg ' \
+              '-y ' \
+              '-f avfoundation ' \
+              '-r 30 ' \
+              '-pixel_format bgr0 ' \
+              '-s 640x480 ' \
+              '-video_device_index 0 ' \
+              '-i ":0" ' \
+              '-c:v libvpx ' \
+              '-b:v 1M ' \
+              '-c:a libvorbis ' \
+              '-b:a 96k ' \
+              '-deadline realtime ' \
+              '-flags +global_header ' \
+              '-cpu-used 1 ' \
+              '-threads 8 ' \
+              '-f segment ' \
+              '-f webm ' \
+              '-'
+
     __connected = False
 
     def __init__(self, host, port):
@@ -40,7 +48,7 @@ class Streamer:
                 time.sleep(3)
 
     def stream(self):
-        p = subprocess.Popen(self.command, stdin=open(os.devnull), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        p = subprocess.Popen(self.command.split(), stdin=open(os.devnull), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         print('Streaming...\n')
         while True:
             data = p.stdout.read(1024)
@@ -71,13 +79,16 @@ class Streamer:
 if __name__ == '__main__':
     try:
         host = sys.argv[1]
+    except IndexError:
+        host = 'localhost'
+
+    try:
         port = int(sys.argv[2])
+    except IndexError:
+        port = 8889
     except ValueError:
         print('Invalid port.')
         exit(1)
-    except IndexError:
-        host = 'localhost'
-        port = 8889
 
     streamer = Streamer(host, port)
     try:
